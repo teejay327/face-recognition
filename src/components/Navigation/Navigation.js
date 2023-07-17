7cb195f04a69d32b334ecd0fbdd202b31aa449e4
teejay327 <mrenergy@hotmail.com>
2023-07-18 00:52:27 +1000

    Added logo & Logo.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,12 @@
 import React from'react';
+import Logo from '../Logo/Logo';
 import './Navigation.css';
 
 const Navigation = () => {
   return (
     <nav className='navbar'>
-      <p className='logo'>Company Logo</p>
+      {/*<p className='logo'>Company Logo</p> */}
+      <Logo className='logo'/>
       <p className='signout-button'>Sign out</p>
     </nav>
   )
