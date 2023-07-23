a5cc5b2ab809002e1ea0f1f555692e35a65889ec
teejay327 <mrenergy@hotmail.com>
2023-07-23 23:49:31 +1000

    CHanged defaults for Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import logo from './AI-Face.jpg';
 const Logo = () => {
   const defaultOptions = {
     reverse:        false,  // reverse the tilt direction
-    max:            35,     // max tilt rotation (degrees)
+    max:            40,     // max tilt rotation (degrees)
     perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
     scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
     speed:          1000,   // Speed of the enter/exit transition
